f98321412d24e603b20ec530b5d27b2a03c7b509
tranj3151 <tranj3151@student.faytechcc.edu>
2024-09-17 22:54:48 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_TranJade.py
@@ -0,0 +1,9 @@
+# Jade Tran
+# 9/17/2024
+# P1LAB1
+# Using Python's input and print functions
+print('Enter your first name:', end=' ')
+First_name = input()
+print('Enter your last name:', end=' ')
+Last_name = input()+'!'
+print('Hello,', First_name, Last_name,'Welcome to CTI-110')
